6c500ea66071f2cb71281aeea71e0eaef70aed60
Oleh <budnyk.oleh@gmail.com>
2022-07-08 17:27:11 +0300

    Implelented copying files

--- DirectoriesCompiler/DC/Main.cpp
+++ DirectoriesCompiler/DC/Main.cpp
@@ -15,17 +15,41 @@ int main(int argc, char* argv[])
 	fs::path inputPath(argv[1]);
 	fs::path outputPath(argv[2]);
 
+	std::cout << std::endl;
 	std::cout << "SOURCE FOLDER = " << inputPath.string() << std::endl;
 	std::cout << "DESTINATION FOLDER = " << outputPath.string() << std::endl;
 	std::cout << std::endl;
 
-	for (auto const& dir : fs::recursive_directory_iterator(inputPath))
+	int filesCount = 0;
+	bool success = true;
+	for (auto& dir : fs::recursive_directory_iterator(inputPath))
 	{
 		if (dir.is_regular_file())
-			std::cout << dir.path().filename() << std::endl;
+		{
+			try
+			{
+				fs::copy(dir, outputPath);
+			}
+			catch (...)
+			{
+				success = false;
+				break;
+			}
 
+			std::cout << "#" << ++filesCount << " copied: " << dir.path().filename() << std::endl;
+		}
 	}
 
-	_getch();
+
+	std::cout << std::endl;
+
+	if (success)
+	{
+		std::cout << "ALL FILES COPIED SUCCESSFULLY!" << std::endl;
+		std::cout << "FILES COUNT : " << filesCount << std::endl;
+	}
+	else
+		std::cout << "SOMETHING WENT WRONG!" << std::endl;
+
 	return 0;
 }
